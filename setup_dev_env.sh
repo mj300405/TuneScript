349a89fcac485528bbddd90af148c74513299fb4
mj300405 <michal-jagoda@o2.pl>
2024-06-11 21:49:27 +0200

    switch to poetry

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -7,26 +7,14 @@ sudo service mysql start
 # Navigate to the project directory
 cd /mnt/c/Users/Michal/Desktop/TuneScript/backend
 
-# Check if the virtual environment exists
-if [ ! -d "venv" ]; then
-  echo "Virtual environment not found. Creating a new virtual environment..."
-  python3 -m venv venv
-fi
+# Install dependencies using Poetry
+echo "Installing dependencies with Poetry..."
+poetry install
 
-# Activate the virtual environment
-echo "Activating virtual environment..."
-source venv/bin/activate
-
-# Check if the virtual environment is activated and Django is installed
-if ! python -c "import django" &> /dev/null; then
-    echo "Django is not installed or the virtual environment is not activated. Installing Django..."
-    pip install django
-fi
-
-# Run Django migrations
+# Run Django migrations using Poetry's environment
 echo "Running Django migrations..."
-python manage.py makemigrations
-python manage.py migrate
+poetry run python manage.py makemigrations
+poetry run python manage.py migrate
 
 # Open VSCode in the project directory
 echo "Opening VSCode..."
